2b12652195fcabaa9a629bf3c8cad60612921f03
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-15 11:07:32 +0700

    [HoangHN] feat: admin login feature

--- src/api/controller/admin/login.rs
+++ src/api/controller/admin/login.rs
@@ -1,7 +1,12 @@
 use axum::Json;
+use once_cell::sync::Lazy;
 use serde::Deserialize;
+use jsonwebtoken::{encode, EncodingKey, Header};
 
-use crate::Result;
+use crate::{api::extractor::JWTAdminClaims, config, Error, Result};
+
+static ENCODING_KEY: Lazy<EncodingKey> = 
+    Lazy::new(|| EncodingKey::from_secret(config::JWT_SECRET.as_bytes()));
 
 #[derive(Deserialize)]
 pub struct LoginData {
@@ -11,6 +16,17 @@ pub struct LoginData {
 
 pub async fn login(Json(data): Json<LoginData>) -> Result<String> {
     // TODO: return jwt token of admin if the username and password is right 
-    
-    todo!()
+    if data.username != config::ADMIN_USERNAME || data.password != config::ADMIN_PASSWORD {
+        return Err(Error::Unauthorized{
+            message: "Username or password is incorrect".to_string(),
+        });
+    }
+    let claims = JWTAdminClaims {
+        sub: data.username.clone(),
+        exp: 60000,
+    };
+
+    let token = encode(&Header::default(), &claims, &ENCODING_KEY);
+
+    Ok(token?)
 }
