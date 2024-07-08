afe9b8f57f9118c46ea8062519e3c4b55b72469e
AkagiYuu <khanghuynh0245@gmail.com>
2024-07-08 15:41:49 +0700

    feat!: remove database

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,26 +1,15 @@
 use std::sync::Mutex;
 
-use sqlx::SqlitePool;
-
-use crate::config;
 use crate::model::room::Room;
-
-const SCHEMA: &str = include_str!("../schema.sql");
 
 #[derive(Debug)]
 pub struct AppState {
-    pub database: SqlitePool,
     pub room: Mutex<Option<Room>>,
 }
 
 impl AppState {
     pub async fn new() -> Self {
-        let pool = SqlitePool::connect(config::DATABASE_URL).await.unwrap();
-
-        sqlx::query(SCHEMA).execute(&pool).await.unwrap();
-
         Self {
-            database: pool,
             room: Mutex::new(None),
         }
     }
--- src/config.rs
+++ src/config.rs
@@ -2,8 +2,6 @@ use chrono_tz::Tz;
 
 pub const SERVER_PORT: u16 = 3000;
 
-pub const DATABASE_URL: &str = "sqlite:data.db";
-
 pub const LOCAL_CORS_DOMAIN: &str = "http://localhost:3000";
 
 pub const PUBLIC_CORS_DOMAIN: &str = "http://localhost:3000";
