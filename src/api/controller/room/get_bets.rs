86c34f60b95d4b8634b3a4a8d172d805c96e7524
AkagiYuu <khanghuynh0245@gmail.com>
2024-07-10 16:25:21 +0700

    update

--- src/api/controller/room/get_bets.rs
+++ src/api/controller/room/get_bets.rs
@@ -10,25 +10,14 @@ use crate::model::room::Room;
 use crate::AppState;
 use crate::Result;
 
-#[derive(TemplateOnce)]
-#[template(path = "bet_infos.stpl")]
-struct BetInfos {
-    data: Vec<BetInfo>,
-}
-
 #[axum::debug_handler]
-pub async fn get_bets(State(state): State<Arc<AppState>>) -> Result<Html<String>> {
+pub async fn get_bets(State(state): State<Arc<AppState>>) -> Result<Json<Option<Vec<BetInfo>>>> {
     let bet_infos = state
         .room
         .lock()
         .map_err(|err| anyhow::anyhow!(err.to_string()))?
         .clone()
-        .map(|room| BetInfos {
-            data: room.bet_infos,
-        })
-        .map(|bet_infos| bet_infos.render_once())
-        .ok_or(anyhow::anyhow!("No room"))?
-        .map_err(anyhow::Error::from)?;
+        .map(|room| room.bet_infos);
 
-    Ok(Html(bet_infos))
+    Ok(Json(bet_infos))
 }
